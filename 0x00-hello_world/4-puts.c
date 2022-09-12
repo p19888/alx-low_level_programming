91bc5e2435e3cb285c04a66db44a0e7abbee62e1
Patrick Dumbuya <pat_bill@ymail.com>
2022-09-12 15:33:13 -0700

    Pato

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /** 
-*a C program that prints exactly Programming is like building a multilingual puzzlturn
+*This  C program that prints exactly Programming is like building a multilingual puzzlturn
+*Description is Okay
 */
- int main(void)
+int main(void)
 {  
     puts("\"Programming is like building a multilingual puzzle");
     return (0);
