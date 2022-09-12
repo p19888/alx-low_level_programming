9f4f01928b3ea263f08c2f8337caaa07c68e3115
Patrick Dumbuya <pat_bill@ymail.com>
2022-09-12 15:19:41 -0700

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/** 
+*a C program that prints exactly "Programming is like building a multilingual puzzle
+*Return
+*/
+
+int main(void)
+{  
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
