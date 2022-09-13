f53834ab90e9a1b567a93145d8e620b1db50c915
Patrick Dumbuya <pat_bill@ymail.com>
2022-09-13 01:54:59 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
 * main: printh the string using prinft function
-* Descripotion: Using the main(void) function
-* A C program that prints exactly with proper grammar, but the outcome is a piece of art
+*
+* Descripotion: A C program that prints exactly with proper grammar, but the outcome is a piece of art
 * return: always give 0 as a return value
 */
 
