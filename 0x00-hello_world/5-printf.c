7c4af168628455210e05331c7248c92bb9d0a783
Patrick Dumbuya <pat_bill@ymail.com>
2022-09-13 01:40:00 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
-*main: printh the string using prinft function
-*Descripotion: Using the main(void) function
-* a C program that prints exactly with proper grammar, but the outcome is a piece of art
-*return: always give 0 as a return value
+* main: printh the string using prinft function
+* Descripotion: Using the main(void) function
+* A C program that prints exactly with proper grammar, but the outcome is a piece of art
+* return: always give 0 as a return value
+*/
 
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art");
-  return(0);
+   printf("with proper grammar, but the outcome is a piece of art");
+   return(0);
 }
