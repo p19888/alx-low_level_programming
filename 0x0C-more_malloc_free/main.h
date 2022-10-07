533397a0bfb39ce0a724109954c3f450b2ce4344
Patrick Dumbuya <pat_bill@ymail.com>
2022-10-07 15:55:37 -0700

    Renew

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,18 +1,11 @@
-#include "main.h"
-#include <stdlib.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * malloc_checked - allocates memory using malloc
- * @b: memory to allocate
- *
- * Return: pointer to allocated memory or normal process termination if error
- */
-void *malloc_checked(unsigned int b)
-{
-	void *p;
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-	p = malloc(b);
-	if (p == NULL)
-		exit(98);
-	return (p);
-}
+#endif /*MAIN_H*/
