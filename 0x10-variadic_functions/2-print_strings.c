f8af6ba8292eb56b46e6c9d46c616730015348a9
Patrick Dumbuya <pat_bill@ymail.com>
2022-10-13 11:30:07 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator const unsigned int n, ...)
                         printf("%s", str);
                 else
                         prinf("(nil)");
-                if (i < n-1 && separator)
+                if (i < n - 1 && separator)
                         printf("%s", separator);
         }
         printf("\n");
